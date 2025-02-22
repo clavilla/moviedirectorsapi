e45148f3657688339da5079a574ad3eacba856b3
clavilla <cristianlavilla@hotmail.com>
2025-02-22 10:43:26 -0300

    Initial commit

---
+++ src/main/java/com/directa24/backendchallenge/moviedirectorsapi/MovieDirectorsApiApplication.java
@@ -0,0 +1,13 @@
+package com.directa24.backendchallenge.MovieDirectorsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDirectorsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDirectorsApiApplication.class, args);
+	}
+
+}
